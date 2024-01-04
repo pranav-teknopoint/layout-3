8ee7cac454e9d577e840ba65535c427f59de2509
pranav-teknopoint <pranav.telangade@teknopoint.in>
2024-01-04 17:32:00 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
-$(document).ready(function(){
-    $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 0,
-        max: 10000,
-        grid: true, 
-        prefix:"$",
-        max_postfix:"+",
-    });
+$(document).ready(function () {
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 10000,
+    grid: true,
+    prefix: "$",
+    max_postfix: "+",
+  });
+
+  $(".pagenumber").slick({
+    slidestoShow: 12,
+    infinite:false,
+    arrows: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
